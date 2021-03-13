ee33d0fca4764b86e8c4f6b8ba7eec2d7f80f0ed
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-03-13 16:48:07 +0800

    chore: uses custom font "Product Sans"

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -1,35 +1,47 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        @hasSection('title')
 
-            <title>@yield('title') - {{ config('app.name') }}</title>
-        @else
-            <title>{{ config('app.name') }}</title>
-        @endif
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    @hasSection('title')
 
-        <!-- Favicon -->
-		<link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
+        <title>@yield('title') - {{ config('app.name') }}</title>
+    @else
+        <title>{{ config('app.name') }}</title>
+    @endif
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
-        @livewireStyles
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
-        <!-- Scripts -->
-        <script src="{{ url(mix('js/app.js')) }}" defer></script>
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ url(mix('css/app.css')) }}">
+    @livewireStyles
+    <style>
+        @font-face {
+            font-family: 'Product Sans';
+            src: url('{{ public_path('fonts/Product Sans Regular.tff') }}');
+        }
 
-        <!-- CSRF Token -->
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-    </head>
+        body {
+            font-family: 'Product Sans', 'Lato', sans-serif;
+        }
+    </style>
 
-    <body>
-        @yield('body')
+    <!-- Scripts -->
+    <script src="{{ url(mix('js/app.js')) }}" defer></script>
 
-        @livewireScripts
-    </body>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+
+<body>
+    @yield('body')
+
+    @livewireScripts
+</body>
+
 </html>
