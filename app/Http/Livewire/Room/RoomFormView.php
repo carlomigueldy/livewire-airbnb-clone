70f8bbfaa6667db1cb0e34defe42ef38ee6ae95e
Carlo Miguel Dy <carlomigueldy@gmail.com>
2021-03-13 16:48:14 +0800

    feat: built the form

--- app/Http/Livewire/Room/RoomFormView.php
+++ app/Http/Livewire/Room/RoomFormView.php
@@ -6,8 +6,99 @@ use Livewire\Component;
 
 class RoomFormView extends Component
 {
+    public $fields = [
+        // 'owner_id' => null,
+        'home_type' => [
+            'value' => null,
+            'type' => 'select',
+            'required' => true,
+            'options' => [
+                'Entire place',
+                'Private room',
+                'Hotel room',
+                'Unique stays',
+                'Superhosts',
+                'Shared room'
+            ]
+        ],
+        'room_type' => [
+            'value' => null,
+            'type' => 'select',
+            'required' => true,
+            'options' => [
+                'Single',
+                'Double',
+                'Triple',
+                'Quad',
+                'King',
+                'Queen',
+                'Twin',
+                'Double-double',
+                'Studio'
+            ]
+        ],
+        'total_occupancy' => [
+            'value' => null,
+            'type' => 'number',
+            'required' => true,
+        ],
+        'total_bedrooms' => [
+            'value' => null,
+            'type' => 'number',
+            'required' => true,
+        ],
+        'total_bathrooms' => [
+            'value' => null,
+            'type' => 'number',
+            'required' => true,
+        ],
+        'summary' => [
+            'value' => null,
+            'type' => 'text',
+            'required' => false,
+        ],
+        'address' => [
+            'value' => null,
+            'type' => 'text',
+            'required' => true,
+        ],
+        'price' => [
+            'value' => null,
+            'type' => 'number',
+            'required' => true,
+        ],
+        'has_tv' => [
+            'value' => null,
+            'type' => 'checkbox',
+            'required' => false,
+        ],
+        'has_kitchen' => [
+            'value' => null,
+            'type' => 'checkbox',
+            'required' => false,
+        ],
+        'has_air_con' => [
+            'value' => null,
+            'type' => 'checkbox',
+            'required' => false,
+        ],
+        'has_heathing' => [
+            'value' => null,
+            'type' => 'checkbox',
+            'required' => false,
+        ],
+
+        // 'latitude' => null,
+        // 'longitude' => null,
+        // 'published_at' => null,
+    ];
+
     public function render()
     {
         return view('livewire.room.room-form-view');
+    }
+
+    public function submit()
+    {
     }
 }
--- resources/views/livewire/room/room-form-view.blade.php
+++ resources/views/livewire/room/room-form-view.blade.php
@@ -1,7 +1,54 @@
 <div>
-    <h1>Room Form View</h1>
+    <div class="flex justify-center align-items-center my-14">
+        <form wire:submit.prevent="submit" class="w-6/12">
+            <div class="text-center ">
+                <h1 class="text-4xl">Room Form View</h1>
+            </div>
+            @foreach ($this->fields as $field => $value)
+                <div class="w-full p-5 ">
+                    <label for="{{ 'fields.' . $field }}">
+                        <span class="font-bold">{{ str_replace('_', ' ', Str::title($field)) }}</span>
+                        <span class="text-red-500">*</span>
+                    </label>
+                    <div class="w-full">
+                        @switch($value['type'])
+                            @case('text')
+                            <input type="text" class="w-full px-3 rounded-md "
+                                wire:model="{{ 'fields.' . $field . '.value' }}">
+                            @break
+                            @case('select')
+                            <select class="w-full rounded-md" wire:model="{{ 'fields.' . $field . '.value' }}">
+                                @foreach ($value['options'] as $option)
+                                    <option value="{{ $option }}">{{ $option }}</option>
+                                @endforeach
+                            </select>
+                            @break
 
-    <form>
+                            @case('number')
+                            <input type="number" min="0" max="10" class="w-full px-3 rounded-md "
+                                wire:model="{{ 'fields.' . $field . '.value' }}">
+                            @break
 
-    </form>
+                            @case('checkbox')
+                            <input type="checkbox" class="rounded focus:outline-none" wire:model="{{ 'fields.' . $field . '.value' }}">
+                            @break
+                            @default
+
+                        @endswitch
+
+                        @error('fields.' . $field . '.value') <span class="error">{{ $message }}</span> @enderror
+                    </div>
+                </div>
+            @endforeach
+
+
+
+            <button class="w-full p-5 mt-10 text-xl text-white bg-indigo-500 rounded-lg shadow-lg color"
+                type="submit">Create</button>
+        </form>
+    </div>
+
+    <code>
+        {{ json_encode($this->fields) }}
+    </code>
 </div>
